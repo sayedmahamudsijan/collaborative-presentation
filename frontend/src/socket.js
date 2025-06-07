c3febd6c021ee223f73a702b7ac306cdd611835e
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-06-08 04:21:52 +0600

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,14 +1,8 @@
 import io from 'socket.io-client';
 
-// Automatically use the deployed backend in production, and localhost during development
-const socket = io(
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000'
-    : 'https://collaborative-presentation.onrender.com',
-  {
-    transports: ['websocket'], // Optional: force WebSocket (avoid polling issues)
-    withCredentials: true,
-  }
-);
+const socket = io('https://collaborative-presentation.onrender.com', {
+  transports: ['websocket'],
+  withCredentials: true
+});
 
 export default socket;
