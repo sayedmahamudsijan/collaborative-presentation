834242d9865e2a80ea0989568bc796248bc0d00b
sayedmahamudsijan <112859381+sayedmahamudsijan@users.noreply.github.com>
2025-06-08 22:55:45 +0600

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -5,4 +5,12 @@ const socket = io('https://collaborative-presentation.onrender.com', {
     withCredentials: true
 });
 
+socket.on('connect', () => {
+    console.log('Socket connected:', socket.id); // Debug log
+});
+
+socket.on('connect_error', (err) => {
+    console.error('Socket connection error:', err.message, err.stack); // Debug log
+});
+
 export default socket;
